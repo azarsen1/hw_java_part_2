1d38202c5f67b46c0c5a9aedda7aa7cd38c0f78b
user <azarsen4@gmail.com>
2025-09-20 20:47:50 +0500

    add HashSetExample and tests

--- src/test/java/pages/HashSetExample.java
+++ src/test/java/pages/HashSetExample.java
@@ -1,2 +1,34 @@
+package pages;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class HashSetExample {
+    HashSet<String> hashSetListWithLogins = new HashSet<>();
+
+    public HashSetExample addUserLogin (String value){
+        hashSetListWithLogins.add(value);
+        System.out.println("Добавили логин " + value + " в базу");
+        return this;
+    }
+
+    public HashSetExample removeUserLogin (String value){
+        hashSetListWithLogins.remove(value);
+        System.out.println("Удалили логин " + value + " из базы");
+        return this;
+    }
+
+    public boolean searchUserLogin (String value){
+        for(String login : hashSetListWithLogins) {
+            if (login.equals(value)){
+                System.out.println("Пользователь с логином " + value + " найден в базе");
+                return true;
+            }
+
+        }
+
+        System.out.println("Пользователь с логином " + value + " не найден в базе");
+        return false;
+
+    }
 }
--- src/test/java/tests/SmokeTestWithCollections.java
+++ src/test/java/tests/SmokeTestWithCollections.java
@@ -2,11 +2,14 @@ package tests;
 
 import pages.ArrayListExample;
 import pages.HashMapExample;
+import pages.HashSetExample;
 
 public class SmokeTestWithCollections {
     public static void main(String[] args) {
 
         ArrayListExample ArrayListExample = new ArrayListExample();
+        System.out.println("База студентов");
+        System.out.println();
         ArrayListExample.addStudentName("Arsen")
                         .addStudentName("Violetta")
                         .addStudentName("Arman")
@@ -16,9 +19,12 @@ public class SmokeTestWithCollections {
         ArrayListExample.searchStudentName("Sergey");
 
         ArrayListExample.removeStudentName("Sergey");
+        System.out.println();
 
 
         HashMapExample HashMapExample = new HashMapExample();
+        System.out.println("Телефонная книга");
+        System.out.println();
         HashMapExample.addUserInfo(1234567891, "Arsen")
                 .addUserInfo(1234567892, "Violetta")
                 .addUserInfo(1234567893, "Arman")
@@ -28,6 +34,25 @@ public class SmokeTestWithCollections {
         HashMapExample.findPhoneNumber("Sergey");
 
         HashMapExample.removePhoneNumber(1234567894);
+        System.out.println();
+
+
+
+        HashSetExample HashSetExample = new HashSetExample();
+        System.out.println("База пользователей");
+        System.out.println();
+        HashSetExample.addUserLogin("Azarsen1")
+                        .addUserLogin("Gyaqsxao")
+                        .addUserLogin("Becefoso")
+                        .addUserLogin("Woqyzica");
+
+
+        HashSetExample.searchUserLogin("Timur");
+        HashSetExample.searchUserLogin("Azarsen1");
+
+        HashSetExample.removeUserLogin("Azarsen1");
+        System.out.println();
 
     }
+
 }
